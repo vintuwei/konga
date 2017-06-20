d710e45aecc452fae74be80f20cbdaf4d2f72e8c
vincent.tuwei <vincent.tuwei@jumo.world>
2017-06-20 16:28:49 +0300

    created consumer api service controller

---
+++ api/controllers/KongConsumerApiPermissionsController.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var _ = require('lodash');
+
+var KongConsumerApiPermissionsController = _.merge(_.cloneDeep(require('../base/KongController')), {
+});
+
+module.exports = KongConsumerApiPermissionsController;
