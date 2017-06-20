df902c76b9e091efaff1d87c14b12ff9335d0b90
vincent.tuwei <vincent.tuwei@jumo.world>
2017-06-20 16:29:13 +0300

    created a controller for onboarding endpoints

---
+++ api/controllers/KongServiceOnboardingController.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var _ = require('lodash');
+
+var KongServiceOnboardingController = _.merge(_.cloneDeep(require('../base/KongController')), {
+});
+
+module.exports = KongServiceOnboardingController;
